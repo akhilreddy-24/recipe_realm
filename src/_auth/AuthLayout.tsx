fb740a96df41c5840870bb958e94902f9848c5c6
akhilreddy-24 <21eg107a24@anurag.edu.in>
2024-06-19 01:04:07 +0530

    first commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,28 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import AnimationComponent from './AnimationComponent'; // Import your AnimationComponent here
+
+const AuthLayout = () => {
+  const isAuthenticated = false; // You can change this to your actual authentication check
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <AnimationComponent /> {/* Display the animation */}
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.jpg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import AuthProvider from "./context/AuthContext";
+import QueryProvider from "./lib/react-query/QueryProvider";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <QueryProvider>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </QueryProvider>
+    </BrowserRouter>
+)
