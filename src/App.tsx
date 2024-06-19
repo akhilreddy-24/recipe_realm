f2fbfe85f87481023da13375fbf41e106f8d6519
akhilreddy-24 <21eg107a24@anurag.edu.in>
2024-06-20 01:01:16 +0530

    seven

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ const App = () => {
         {/*public routes */}
         
               <Route element={<AuthLayout />}>
-                <Route path="/sign-in" element={<SigninForm />} />
-                <Route path="/sign-up" element={<SignupForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+          <Route path="/sign-in" element={<SigninForm />} />
               </Route>
         
         {/*private routes */}
