04074c3901a276ad2a1a8bb8360782f01b57778d
akhilreddy-24 <21eg107a24@anurag.edu.in>
2024-06-20 22:29:29 +0530

    comeon

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -50,9 +50,8 @@ const SignupForm = () => {
 
       if (!session) {
         toast({ title: "Something went wrong. Please login your new account", });
-        
-        navigate("/sign-in");
-        
+
+        // Removed automatic redirection to sign-in page
         return;
       }
 
@@ -60,7 +59,6 @@ const SignupForm = () => {
 
       if (isLoggedIn) {
         form.reset();
-
         navigate("/");
       } else {
         toast({ title: "Login failed. Please try again.", });
@@ -75,8 +73,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-      <img src="/assets/images/recipe.jpg" alt="logo" className="w-20"/>
-
+        <img src="/assets/images/recipe.jpg" alt="logo" className="w-20"/>
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
