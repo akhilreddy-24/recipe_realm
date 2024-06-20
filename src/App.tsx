5b7d24df250f35e73045dc58b6d8aa9a2abceb2f
akhilreddy-24 <21eg107a24@anurag.edu.in>
2024-06-20 22:41:44 +0530

    yess

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
 import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
-
-
 import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
   return (
-      <main className='flex-h-screen'>
-          <Routes>
-        {/*public routes */}
-        
-              <Route element={<AuthLayout />}>
-          
+    <main className='flex-h-screen'>
+      <Routes>
+        {/* Redirect to /sign-up by default */}
+        <Route path="/" element={<Navigate to="/sign-up" />} />
+
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
-              </Route>
-        
-        {/*private routes */}
-              <Route element={<RootLayout/>}>
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
           <Route index element={<Home />} />
-          <Route path="/explore" element={<Explore/>} />
-          <Route path="/saved" element={<Saved/>} />
-          <Route path="/all-users" element={<AllUsers/>} />
-          <Route path="/create-post" element={<CreatePost/>} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
           <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/profile/:id/*" element={<Profile />} />
           <Route path="/update-profile/:id" element={<UpdateProfile />} />
-              </Route>
+        </Route>
       </Routes>
       <Toaster />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
